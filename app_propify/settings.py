034d00fb0aa0443a0af8083c1779f851771bebef
Ramiro <ramsbr360@gmail.com>
2026-03-20 13:53:09 -0500

    wip static root

--- app_propify/settings.py
+++ app_propify/settings.py
@@ -191,6 +191,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # ---------------------------------------------------------------------------
 # Azure Blob Storage / Media
