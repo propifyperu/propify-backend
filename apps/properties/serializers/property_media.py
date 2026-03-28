270d97d77d3efcd117e36f4aebf90a55db2d8559
Ramiro <ramsbr360@gmail.com>
2026-03-28 12:45:52 -0500

    refactor endpoints propertymedia and propertydocuments

--- apps/properties/serializers/property_media.py
+++ apps/properties/serializers/property_media.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.properties.models import PropertyMedia
+from apps.properties.models import Property, PropertyMedia
 
 
 class PropertyMediaSerializer(serializers.ModelSerializer):
@@ -8,3 +8,35 @@ class PropertyMediaSerializer(serializers.ModelSerializer):
         model = PropertyMedia
         fields = "__all__"
         read_only_fields = ["id", "created_at", "updated_at"]
+
+
+class PropertyWithMediaSerializer(serializers.ModelSerializer):
+    property_type_name = serializers.CharField(source="property_type.name", read_only=True, allow_null=True, default=None)
+    property_subtype_name = serializers.CharField(source="property_subtype.name", read_only=True, allow_null=True, default=None)
+    operation_type_name = serializers.CharField(source="operation_type.name", read_only=True, allow_null=True, default=None)
+    currency_code = serializers.CharField(source="currency.code", read_only=True, allow_null=True, default=None)
+    property_status_name = serializers.CharField(source="property_status.name", read_only=True, allow_null=True, default=None)
+    media = PropertyMediaSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Property
+        fields = [
+            "id",
+            "code",
+            "title",
+            "price",
+            "property_type",
+            "property_type_name",
+            "property_subtype",
+            "property_subtype_name",
+            "operation_type",
+            "operation_type_name",
+            "currency",
+            "currency_code",
+            "property_status",
+            "property_status_name",
+            "map_address",
+            "display_address",
+            "responsible",
+            "media",
+        ]
