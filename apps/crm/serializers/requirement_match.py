7418fb6f6148990a615ec2a6ad80fd69989c4377
Ramiro <ramsbr360@gmail.com>
2026-04-10 10:19:55 -0500

    add price and district_name in endpoin requirement_match

--- apps/crm/serializers/requirement_match.py
+++ apps/crm/serializers/requirement_match.py
@@ -6,6 +6,8 @@ class RequirementMatchSerializer(serializers.ModelSerializer):
     property_title = serializers.CharField(source="property.title", read_only=True)
     property_code = serializers.CharField(source="property.code", read_only=True)
     property_main_image = serializers.SerializerMethodField()
+    property_price = serializers.DecimalField(source="property.price", max_digits=12, decimal_places=2, read_only=True)
+    property_district_name = serializers.CharField(source="property.district.name", read_only=True, allow_null=True, default=None)
 
     def get_property_main_image(self, obj):
         media = obj.property.media.filter(order=1).first()
@@ -17,6 +19,6 @@ class RequirementMatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequirementMatch
         fields = [
-            "id", "requirement", "property", "property_code", "property_title",
+            "id", "requirement", "property", "property_code", "property_title","property_price","property_district_name",
             "property_main_image", "score", "details", "computed_at", "is_active",
         ]
